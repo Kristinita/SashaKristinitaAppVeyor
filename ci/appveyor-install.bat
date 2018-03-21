3b930b3ddd5c6494aad590f077b2dee0a339a2bc
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-03-21 14:31:09 +0300

    ci(appveyor): Try disable pip upgrading

--- ci/appveyor-install.bat
+++ ci/appveyor-install.bat
@@ -1,7 +1,7 @@
 @REM @Author: Kristinita
 @REM @Date:   2018-02-28 19:42:16
 @REM @Last Modified by:   Kristinita
-@REM Modified time: 2018-03-04 17:12:45
+@REM Modified time: 2018-03-21 14:30:39
 @REM Parallel pip and npm commands.
 @REM Use REM comments:
 @REM https://stackoverflow.com/a/12407934/5951529
@@ -16,5 +16,5 @@
 @REM https://github.com/majkinetor/au-packages/issues/75
 @REM https://ci.appveyor.com/project/Kristinita/sashatidydebugging/build/1.0.12
 START /B CMD /C "choco install html-tidy -y -ignoredependencies"
-START /B CMD /C "pip install --upgrade pip & pip install pipenv & pipenv install --dev"
+START /B CMD /C "pip install pipenv & pipenv install --dev"
 START /B CMD /C "choco install nodejs -y & npm install -g grunt-cli & npm install"
